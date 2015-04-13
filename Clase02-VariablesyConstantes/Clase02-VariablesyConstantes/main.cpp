56bae5e270f5e3ba6846f6802505c3475c13b0d7
sergio pita <pita310593@gmail.com>
2015-04-13 19:12:12 +0200

    Clase 02

---
+++ Clase02-VariablesyConstantes/Clase02-VariablesyConstantes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase02-VariablesyConstantes
+//
+//  Created by Sergio Pita on 13/4/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
