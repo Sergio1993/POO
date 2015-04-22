50b8064d3dacacfefe716c56bf7f6050620f44b9
Sergio Pita <pita310593@gmail.com>
2015-04-22 18:04:59 +0200

    clase 22

--- Clase22_UML/Clase22_UML/main.cpp
+++ Clase22_UML/Clase22_UML/main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
+#include <string.h>
+using namespace std;
+string mayusculas(string &resp){
+    
+    for(int i = 0; i < resp.length(); i++){
+        resp[i] = toupper(resp[i]);
+    }
+    
+    return resp;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    //TAREA 1
+    bool lamparaEnchufada = true;
+    bool ampolletaQuemada = false;
+    string respuestaUno = "";
+    string respuestaDos = "";
+    
+    cout << "¿La lampara esta enchufada?" << endl;
+    cin >> respuestaUno;
+    
+    mayusculas(respuestaUno);
+
+    if (respuestaUno == "SI"){
+        
+        lamparaEnchufada = true;
+        cout << "¿Esta quemada la ampolleta?" << endl;
+        cin >> respuestaDos;
+        
+    }else if (respuestaUno == "NO"){
+        
+        lamparaEnchufada = false;
+        cout << "Enchufar la lampara" << endl;
+    }
+    
+    mayusculas(respuestaDos);
+    
+    if (respuestaDos == "SI"){
+        
+        ampolletaQuemada = true;
+        cout << "Cambiar la ampolleta" << endl;
+        
+    }else if (respuestaDos == "NO"){
+        
+        ampolletaQuemada = false;
+        cout << "Comprar la lampara" << endl;
+        
+    }
+
     return 0;
 }
