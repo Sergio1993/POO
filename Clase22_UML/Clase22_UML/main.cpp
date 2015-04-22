7f43579cb62b9766110911ef1f2deb03cd5dd775
Sergio Pita <pita310593@gmail.com>
2015-04-22 17:18:56 +0200

    Clase22_uml

---
+++ Clase22_UML/Clase22_UML/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase22_UML
+//
+//  Created by Sergio Pita on 22/4/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
