e9d3d4b0a5eaf5f520ca64f0e1aa7a8fe354c7e1
Sergio Pita <pita310593@gmail.com>
2015-04-29 18:24:23 +0200

    Clase 23

---
+++ Clase23-Herencia/Clase23-Herencia/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Clase23-Herencia
+//
+//  Created by Sergio Pita on 29/4/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
