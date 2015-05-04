68f88856a82224ef628b3bb1e9d74f4d19389395
Sergio Pita <pita310593@gmail.com>
2015-05-04 19:27:16 +0200

    Eliminación clases de 3 a8 y añadir de nuevo la clase3

---
+++ Clase03-Funciones/Clase03-Funciones/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Operadores
+//
+//  Created by Sergio Pita on 16/12/14.
+//  Copyright (c) 2014 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int a=14; int b=9;
+    
+    int c= a>b ? a : b;
+    
+    cout << c << endl;
+    cout << (a>b ? a:b) << endl;
+    
+    return 0;
+}
+
