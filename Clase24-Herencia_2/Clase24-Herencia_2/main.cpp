4cc7b05a9ddb17475bd1c92cc0574bbbf0d04839
Sergio Pita <pita310593@gmail.com>
2015-05-06 20:24:16 +0200

    Clase24

---
+++ Clase24-Herencia_2/Clase24-Herencia_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase24-Herencia_2
+//
+//  Created by Sergio Pita on 6/5/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
