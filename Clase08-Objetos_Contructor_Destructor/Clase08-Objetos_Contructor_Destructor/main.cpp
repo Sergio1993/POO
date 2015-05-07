ecd70468a9deb50f0148c2d07ea471b7feb9f9b5
Sergio Pita <pita310593@gmail.com>
2015-05-07 19:35:24 +0200

    Clase08

---
+++ Clase08-Objetos_Contructor_Destructor/Clase08-Objetos_Contructor_Destructor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase08-Objetos_Contructor_Destructor
+//
+//  Created by Sergio Pita on 7/5/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
