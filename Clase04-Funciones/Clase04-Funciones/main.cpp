bb918129b3fcde76dc5a97262cfe6f0d8bfbeab3
Sergio Pita <pita310593@gmail.com>
2015-05-07 19:20:24 +0200

    Clase04-Funciones

---
+++ Clase04-Funciones/Clase04-Funciones/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase04-Funciones
+//
+//  Created by Sergio Pita on 7/5/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
