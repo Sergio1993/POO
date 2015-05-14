9144e358de19a928223d22ade24027fe31b97232
Sergio Pita <pita310593@gmail.com>
2015-05-14 17:26:14 +0200

    Clase25

---
+++ Clase25-Polimorfismo/Clase25-Polimorfismo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase25-Polimorfismo
+//
+//  Created by Sergio Pita on 14/5/15.
+//  Copyright (c) 2015 Sergio Pita. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
